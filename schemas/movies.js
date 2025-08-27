ebf53762154ba9f9644ccf0b69977bf0b9993fa1
unknown <Redolff>
2025-08-27 02:19:40 -0300

    adaptation the validations movie with zod

--- schemas/movies.js
+++ schemas/movies.js
@@ -3,12 +3,14 @@ import z from 'zod'
 // Schema de validaciones para las movies
 const movieSchema = z.object({
     title: z.string(),
-    year: z.number().int().min(1900).max(2026),
+    year: z.preprocess((val) => Number(val), z.number().int().min(1900).max(2026)),
     director: z.string(),
-    duration: z.number().int().positive(),
+    duration: z.preprocess((val) => Number(val), z.number().int().positive()),
     poster: z.string().url(),
-    genre: z.enum([ "Drama", "Adventure", "Sci-Fi", "Action", "Crime", "Romance", "Animation", "Biography", "Fantasy"]).array(),
-    rate: z.number().positive().max(10)
+    genre: z.enum(["Drama", "Deporte", "History", "Thriller", "Adventure", "Sci-Fi", "Action",
+        "Crime", "Romance", "Animation", "Biography", "Fantasy", "Mystery", "War", "Horror"
+    ]).array(),
+    rate: z.preprocess((val) => Number(val), z.number().positive().max(10))
 })
 
 export const validateMovie = (object) => {
